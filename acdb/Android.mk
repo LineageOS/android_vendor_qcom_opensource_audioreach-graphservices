988407a8a0a2ab9ee7eb34f06fa0f5484e923bce
Cosmin Tanislav <demonsingur@gmail.com>
2025-01-26 15:34:39 +0200

    Remove usage of comdef.h
    Change-Id: Id5a79122f0dd4e523d425cf2567ec855ef244849

--- acdb/Android.mk
+++ acdb/Android.mk
@@ -78,6 +78,8 @@ ifeq ($(strip $(AUDIO_FEATURE_ENABLED_MCS)),true)
 	LOCAL_CFLAGS += -DMCS_ENABLED
 endif
 
+LOCAL_CFLAGS += -DATS_USES_DUMMY_DIAG
+
 LOCAL_C_INCLUDES := \
     $(LOCAL_PATH)/ats/inc \
     $(LOCAL_PATH)/ats/rtc/common/inc \
@@ -87,6 +89,7 @@ LOCAL_C_INCLUDES := \
     $(LOCAL_PATH)/ats/fts/common/inc/ \
     $(LOCAL_PATH)/ats/transports/diag/linux/actp/inc/ \
     $(LOCAL_PATH)/ats/transports/diag/linux/audtp/inc/ \
+    $(LOCAL_PATH)/ats/transports/diag/linux/inc \
     $(LOCAL_PATH)/ats/adie/common/inc \
     $(LOCAL_PATH)/ats/adie/common/api \
     $(LOCAL_PATH)/ats/api \
@@ -131,14 +134,12 @@ LOCAL_CFLAGS += \
 LOCAL_HEADER_LIBRARIES := \
     libcutils_headers \
     libutils_headers \
-    libdiag_headers \
     vendor_common_inc
 
 LOCAL_SHARED_LIBRARIES := \
     liblx-osal\
     libutils\
     libcutils \
-    libdiag \
     libar-gsl\
     libar-acdb
 
